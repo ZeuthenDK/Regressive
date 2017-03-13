b061953ce7976275b2ccca09d216ba50c07fb971
VoidTelekinesist <bjoernak@gmail.com>
2017-03-13 08:28:04 +0100

    Added Checkbox dialouge for "Show Grid", that proves it able to update and read its value. Coordinates are now shown when hovering over the graph area. Not sure how to get actual coordinates from the graph though.

--- Regressive/Regressive/MainWindow.xaml.cs
+++ Regressive/Regressive/MainWindow.xaml.cs
@@ -24,5 +24,27 @@ namespace Regressive
 		{
 			InitializeComponent();
 		}
+
+		//Event when clicking, or changing with the keyboard, the "Show Grid" Checkbox
+		private void Show_Grid_Click(object sender, RoutedEventArgs e)
+		{
+			//IsChecked is the type 'bool?', which can be null. 'Show_Grid.IsChecked ?? true' returns 'true' if the value is null
+			if (Show_Grid.IsChecked ?? true)
+			{
+				MessageBox.Show("You Clicked on \"Show Grid\". It is now showing the grid");
+			}
+			else
+			{
+				MessageBox.Show("You Clicked on \"Show Grid\". It is now hiding the grid");
+			}
+			
+		}
+
+		private void Graph_Area_MouseMove(object sender, MouseEventArgs e)
+		{
+			string x = Math.Round((e.GetPosition(Graph_Area).X - 0.2), 1).ToString();
+			string y = Math.Round((e.GetPosition(Graph_Area).Y - 0.2), 1).ToString();
+			Coordinates_Display.Text = x + ";" + y;
+		}
 	}
 }
